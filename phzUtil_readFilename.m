66b414a786b5f5977947705c68793948105ffee1
Gabe Nespoli <gabenespoli@gmail.com>
2016-08-02 15:04:36 -0400

    moved readFilename (namestr) to its own function for use outside of phz_create

---
+++ phzUtil_readFilename.m
@@ -0,0 +1,72 @@
+function groupingVars = phzUtil_readFilename(filename,namestr)
+if isempty(namestr), return, end
+
+% create empty output
+groupingVars = struct(...
+    'study',        '',...
+    'datatype',     '',...
+    'participant',  '',...
+    'group',        '',...
+    'condition',    '',...
+    'session',      '');
+
+% find out which grpvars are in namestr & their order & their start/ends
+vars = {'study','datatype','participant','group','condition','session'};
+varsbeg = sort(cell2mat(regexp(namestr,vars,'start')));
+varsend = sort(cell2mat(regexp(namestr,vars,'end')));
+grpvars = cell(size(varsbeg));
+for i = 1:length(grpvars), grpvars{i} = namestr(varsbeg(i):varsend(i)); end
+
+% use start/ends to get delimstr that occur before & after each grpvar
+% i.e., length(delimstr) = length(grpvars) + 1
+delimstr = cell(1,length(grpvars) + 1);
+for i = 1:length(delimstr)
+    switch i
+        
+        case 1
+            if varsbeg(i) == 1
+                delimstr{i} = '';
+            else
+                delimstr{i} = namestr(1:varsbeg(i) - 1);
+            end
+            
+        case length(delimstr)
+            if varsend(i-1) == length(grpvars)
+                delimstr{i} = ''; 
+            else
+                delimstr{i} = namestr(varsend(i-1) + 1:end);
+            end
+            
+        otherwise
+            delimstr{i} = namestr(varsend(i-1) + 1:varsbeg(i) - 1);
+    end
+end
+
+if any(cellfun(@isempty,delimstr(2:end-1)))
+    error('There must be some delimiter between grouping variables in the filename.'), end
+
+[~,name,~] = fileparts(filename);
+
+% move cursor past leading delimiter
+if isempty(delimstr{1}), cursor = 1;
+elseif strcmp(delimstr{1},name(1:length(delimstr{1})))
+    cursor = 1 + length(delimstr{1});
+else error(['Filename ''',name,''' doesn''t match the namestr.'])
+end
+delimstr(1) = []; % delimstr now holds delims that follow each grpvar
+
+% loop through grpvars and get their vals from the filename
+for i = 1:length(grpvars)
+    
+    if i == length(grpvars) && isempty(delimstr{i})
+        groupingVars.(grpvars{i}) = name(cursor:end);
+        
+    else
+        strend = min(strfind(name(cursor:end),delimstr{i}));
+        groupingVars.(grpvars{i}) = name(cursor:cursor + strend - 2);
+        cursor = cursor + strend + length(delimstr{i}) - 1;
+    end
+end
+
+
+end
--- phz_create.m
+++ phz_create.m
@@ -226,8 +226,10 @@ for i = 1:length(files)
     raw = load(PHZ.proc.create.datafile,'-mat');
     
     % get grouping vars from filename or parameter/value pairs
-    PHZ = readFilename(PHZ,namestr);
+    temp = phzUtil_readFilename(PHZ.proc.create.datafile,namestr);
+    PHZ.proc.create.namestr = namestr;
     for j = {'study','datatype','participant','group','condition','session'}; field = j{1};
+        if ~isempty(temp.(field)), PHZ.(field) = temp.(field); end
         if ~isempty(eval(field)), PHZ.(field) = eval(field); end
     end
     
@@ -272,70 +274,6 @@ for i = 1:length(files)
     end
      
 end
-end
-
-function PHZ = readFilename(PHZ,namestr)
-if isempty(namestr), return, end
-
-% find out which grpvars are in namestr & their order & their start/ends
-vars = {'study','datatype','participant','group','condition','session'};
-varsbeg = sort(cell2mat(regexp(namestr,vars,'start')));
-varsend = sort(cell2mat(regexp(namestr,vars,'end')));
-grpvars = cell(size(varsbeg));
-for i = 1:length(grpvars), grpvars{i} = namestr(varsbeg(i):varsend(i)); end
-
-% use start/ends to get delimstr that occur before & after each grpvar
-% i.e., length(delimstr) = length(grpvars) + 1
-delimstr = cell(1,length(grpvars) + 1);
-for i = 1:length(delimstr)
-    switch i
-        
-        case 1
-            if varsbeg(i) == 1
-                delimstr{i} = '';
-            else
-                delimstr{i} = namestr(1:varsbeg(i) - 1);
-            end
-            
-        case length(delimstr)
-            if varsend(i-1) == length(grpvars)
-                delimstr{i} = ''; 
-            else
-                delimstr{i} = namestr(varsend(i-1) + 1:end);
-            end
-            
-        otherwise
-            delimstr{i} = namestr(varsend(i-1) + 1:varsbeg(i) - 1);
-    end
-end
-
-if any(cellfun(@isempty,delimstr(2:end-1)))
-    error('There must be some delimiter between grouping variables in the filename.'), end
-
-[~,name,~] = fileparts(PHZ.proc.create.datafile);
-
-% move cursor past leading delimiter
-if isempty(delimstr{1}), cursor = 1;
-elseif strcmp(delimstr{1},name(1:length(delimstr{1})))
-    cursor = 1 + length(delimstr{1});
-else error(['Filename ''',name,''' doesn''t match the namestr.'])
-end
-delimstr(1) = []; % delimstr now holds delims that follow each grpvar
-
-% loop through grpvars and get their vals from the filename
-for i = 1:length(grpvars)
-    
-    if i == length(grpvars) && isempty(delimstr{i})
-        PHZ.(grpvars{i}) = name(cursor:end);
-        
-    else
-        strend = min(strfind(name(cursor:end),delimstr{i}));
-        PHZ.(grpvars{i}) = name(cursor:cursor + strend - 2);
-        cursor = cursor + strend + length(delimstr{i}) - 1;
-    end
-end
-
-PHZ.proc.create.namestr = namestr;
 end
 
 function PHZ = getBlankPHZ(verbose)
