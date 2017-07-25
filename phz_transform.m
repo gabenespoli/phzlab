68b4986ce7eef01ff2dee386b6e601ea5836074f
Gabe Nespoli <gabenespoli@gmail.com>
2017-07-25 11:35:02 -0400

    add new units to the history string

--- phz_transform.m
+++ phz_transform.m
@@ -2,15 +2,15 @@
 %
 % USAGE
 %   PHZ = phz_transform(PHZ,transform)
-% 
+%
 % INPUT
 %   PHZ       = PHZLAB data structure.
-% 
-%   transform = [string|numeric|cell] Type of transformation to apply to the 
+%
+%   transform = [string|numeric|cell] Type of transformation to apply to the
 %               data. See below for a list of possible transformations. If
 %               a cell, the first element is the transformation to apply,
 %               and the second element is a string with the new units.
-% 
+%
 %       'sqrt'    = Take square root of every data point.
 %                   Data cannot be negative.
 %       '^2'      = Square every data point.
@@ -18,24 +18,24 @@
 %       'log10'   = Base 10 logarithm.
 %       'log2'    = Base 2 logarithm.
 %       [numeric] = Multiply each data point by this number.
-% 
+%
 % OUTPUT
 %   PHZ.data  = The transformed data.
 %   PHZ.proc.transform.transform = The transformation(s) performed.
 %   PHZ.proc.transform.
 
 % Copyright (C) 2016 Gabriel A. Nespoli, gabenespoli@gmail.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
 
@@ -51,9 +51,11 @@ if nargin < 3
 if ~iscell(transform)
     transform = {transform}; end
 
+newUnits = false;
 if length(transform) > 1
     if ischar(transform{2})
         PHZ.units = transform{2};
+        newUnits = true;
     else
         warning('New units must be a string. Ignoring new units.')
     end
@@ -68,7 +70,7 @@ if ismember('rej',fieldnames(PHZ.proc))
 else
     do_rej = false;
 end
-    
+
 % apply transformation
 if isnumeric(transform{1})
     
@@ -123,8 +125,11 @@ else
     end
 end
 
+if newUnits
+    transformStr = [transformStr, ' Changed units to ', PHZ.units, '.'];
+end
 PHZ = phz_history(PHZ,transformStr,verbose);
-PHZ.proc.transform = transform;
-PHZ.units = newUnits;
+procName = phzUtil_getUniqueProcName(PHZ,'transform');
+PHZ.proc.(procName) = transform;
 
 end
