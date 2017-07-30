6918ae298e8feb1fb395bea90122c21e2eec01bc
Gabe Nespoli <gabenespoli@gmail.com>
2017-07-30 15:28:06 -0400

    first draft of rewrite for marking instead of actually rejecting trials

--- phz_reject.m
+++ phz_reject.m
@@ -2,33 +2,33 @@
 %
 % USAGE    
 %   PHZ = PHZ_REJECT(PHZ,threshold)
-%   PHZ = PHZ_REJECT(PHZ,threshold,rejtype)
 % 
 % INPUT   
 %   PHZ       = [struct] PHZLAB data structure.
 % 
 %   threshold = [numeric|string] Trials with any value exceeding this 
 %               value of THRESHOLD will be rejected. Enter 0 to unreject 
-%               all trials. THRESHOLD can be a string as a shorthand for
-%               setting REJTYPE to 'sd'. In this case the string must be a
-%               number followed by 'sd' (e.g., '0.05sd').
-% 
-%   rejtype   = ['value'|'sd'] The units of THRESHOLD. Default is 'value' 
-%               (i.e., the same units as PHZ.units). Enter 'sd' to reject 
-%               a trial if any value exceeds a THRESHOLD Number of that 
+%               all trials. THRESHOLD can be a string in order to reject
+%               by standard deviation. In this case the string must be a
+%               number followed by 'sd' (e.g., '0.05sd'). A trial will be
+%               rejected if any value exceeds a THRESHOLD number of that
 %               trial's standard deviation.
+%
+%               Note that trials can be rejected manually with
+%               phz_plotTrials. This function creates a field
+%               PHZ.proc.reject.manual with these rejections. Calling
+%               phz_reject with threshold=0 or threshold=<other value>
+%               will not erase these manual rejections. Use
+%               threshold='reset' to clear all rejections.
+
 %                       
 % OUTPUT  
 %   PHZ.proc.reject.threshold   = The value specified in THRESHOLD.
 %   PHZ.proc.reject.units       = The units of the threshold value.
-%   PHZ.proc.reject.locs        = Indices of rejected trials.
-%   PHZ.proc.reject.data_locs   = Indices of retained trials.
+%   PHZ.proc.reject.ind         = Indices of rejected trials.
 % 
 % EXAMPLES
 %   PHZ = phz_reject(PHZ,20)     >> Reject all trials with a value > 20.
-% 
-%   PHZ = phz_reject(PHZ,3,'sd') >> Reject trials with a value > 3 standard
-%                                deviations from the mean of all trials.
 % 
 %   PHZ = phz_reject(PHZ,'3sd')  >> Reject trials with a value > 3 standard
 %                                deviations from the mean of all trials.
@@ -50,169 +50,83 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
 
-function PHZ = phz_rej(PHZ,threshold,varargin)
+function PHZ = phz_reject(PHZ,threshold,verbose)
 
-if nargout == 0 && nargin == 0, help phz_rej, return, end
+if nargout == 0 && nargin == 0, help phz_reject, return, end
 if nargin > 1 && isempty(threshold), return, end
+if nargin < 3, verbose = true; end
 
-% defaults
-rejtype = 'value'; % 'value' or 'sd'
-verbose = true;
-
-% user-defined
-for i = 1:length(varargin)
-    
-    if isnumeric(varargin{i}) || islogical(varargin{i})
-        verbose = varargin{i};
-        
-    elseif ischar(varargin{i})
-        rejtype = varargin{i};
-        
-    end
+% check that no other processing has been done since phz_reject
+names = fieldnames(PHZ.proc);
+if ismember('reject', names) && ~strcmp(names{end}, 'reject')
+    error(['Other processing has been done since threshold ',...
+        'rejection. Cannot undo previous rejections.'])
 end
 
-% check input
-if ischar(threshold) && strcmpi(threshold(end-1:end),'sd')
-    rejtype = 'sd';
-    threshold = str2double(threshold(1:end-2));
-elseif ~isnumeric(threshold), error('Problem with THRESHOLD.')
-end
+% get threshold value and rejection type
+if isnumeric(threshold)
+    units = PHZ.units;
 
-% get indices of artifacts and copy them to PHZ.rej.locs
-[PHZ,do_rej,do_restore] = verifyREJinput(PHZ,threshold,verbose);
+elseif ischar(threshold) 
+    if strcmpi(threshold(end-1:end),'sd')
+        units = 'SD';
+        threshold = str2double(threshold(1:end-2));
 
-if do_rej || do_restore
-    
-    if do_restore
-
-        % check that no other processing has been done since phz_rej
-        names = fieldnames(PHZ.proc);
-        if ~strcmp(names{end},'rej')
-            error(['Other processing has been done since threshold ',...
-                'rejection. Cannot undo previous rejections.'])
-        end
-        
-        % concatenate all locs, data, and grouping vars
-        locs        = [PHZ.proc.rej.data_locs;    PHZ.proc.rej.locs];
-        data        = [PHZ.data;                  PHZ.proc.rej.data];
-        participant = [PHZ.meta.tags.participant; PHZ.proc.rej.participant];
-        condition   = [PHZ.meta.tags.condition;   PHZ.proc.rej.condition];
-        group       = [PHZ.meta.tags.group;       PHZ.proc.rej.group];
-        session     = [PHZ.meta.tags.session;     PHZ.proc.rej.session];
-        trials      = [PHZ.meta.tags.trials;      PHZ.proc.rej.trials];
-        
-        % sort locs, then sort others based on it
-        [~,ind]                   = sort(locs);
-        PHZ.data                  = data(ind,:);
-        
-        for i = {'participant','group','condition','session','trials'}
-            field = i{1};
-            if ~isempty(PHZ.meta.tags.(field))
-                temp = eval(field);
-                PHZ.meta.tags.(field) = temp(ind);
-            end
-        end
-        
-        PHZ.proc = rmfield(PHZ.proc,'rej');
-        PHZ = phz_history(PHZ,'Unrejected all trials.',verbose);
+    elseif strcmpi(threshold, 'reset')
+        PHZ.proc = rmfield(PHZ.proc, 'reject');
+        PHZ = phz_history(PHZ, ['All trials marked for rejection ', ...
+            'were unmarked for rejection (even manual rejections).'], ...
+            verbose);
     end
-    
-    if do_rej
-        
-        PHZ = getRejStructure(PHZ);
-        
-        % do actual finding of trials to reject
-        if strcmp(rejtype,PHZ.units), rejtype = 'threshold'; end
-        switch lower(rejtype)
-            case {'sd','std','stdev'}
-                PHZ.proc.rej.units = 'SD';
-                stDev = std(PHZ.data(:));
-                for i = 1:size(PHZ.data,1)
-                    if max(abs(PHZ.data(i,:))) > stDev * threshold
-                        PHZ.proc.rej.locs = [PHZ.proc.rej.locs; i];
-                    end
-                end
-                
-            case {'threshold','value'}
-                PHZ.proc.rej.units = PHZ.units;
-                for i = 1:size(PHZ.data,1)
-                    if max(abs(PHZ.data(i,:))) > threshold
-                        PHZ.proc.rej.locs = [PHZ.proc.rej.locs; i];
-                    end
-                end
-        end
-        
-        % check that ~reject all trials or ~reject no trials
-        if length(PHZ.proc.rej.locs) == size(PHZ.data,1)
-            disp('! This threshold would reject all trials. Aborting phz_rej.m...')
-            PHZ.proc = rmfield(PHZ.proc,'rej');
-            disp(['  The min threshold to reject a trial in this dataset is ', ...
-                num2str(min(max(abs(PHZ.data),[],2))),'.'])
-            return
-        end
-        
-        if isempty(PHZ.proc.rej.locs)
-            disp(['  This threshold would not reject any trials. ', ...
-                'Aborting phz_rej.m...'])
-            PHZ.proc = rmfield(PHZ.proc,'rej');
-            disp(['  The max value in this dataset is ', ...
-                num2str(max(abs(PHZ.data(:)))),'.'])
-            return
-        end
-        
-        % copy tags over to PHZ.rej.(field)
-        for i = {'participant','group','condition','session','trials'}, field = i{1};
-            if ~isempty(PHZ.meta.tags.(field))
-                PHZ.proc.rej.(field) = PHZ.meta.tags.(field)(PHZ.proc.rej.locs);
-                PHZ.meta.tags.(field)(PHZ.proc.rej.locs) = [];
-            end
-        end
-        PHZ.proc.rej.data = PHZ.data(PHZ.proc.rej.locs,:);
-        PHZ.data(PHZ.proc.rej.locs,:) = [];
-        PHZ.proc.rej.data_locs(PHZ.proc.rej.locs) = [];
-        
-        % misc updates to PHZ structure
-        PHZ.proc.rej.threshold = threshold;
-        
-        % add to PHZ.history
-        historyThreshold = [num2str(threshold),' ',PHZ.proc.rej.units];
-        PHZ = phz_history(PHZ,['Threshold of ',historyThreshold,...
-            ' rejected ',num2str(length(PHZ.proc.rej.locs)),' / ',...
-            num2str(length(PHZ.proc.rej.data_locs) + length(PHZ.proc.rej.locs)),...
-            ' trials (',num2str(round(phz_rejrate(PHZ,'%'),1)),'%).'],verbose);
-        
-    end
-end
-end
 
-function [PHZ,do_rej,do_restore] = verifyREJinput(PHZ,threshold,verbose)
-
-if threshold == 0
-    
-    % newThresh == 0, oldThresh == 0 (do nothing and return)
-    if ~ismember('rej',fieldnames(PHZ.proc))
-        do_restore = 0;
-        do_rej = 0;
-        if verbose, disp('Threshold is already set to 0.'), end
-        
-    else % newThresh == 0, oldThresh == val
-        do_restore = true;
-        do_rej = false;
-        
-    end
-    
 else
-    % newThresh == val, oldThresh == val
-    if ismember('rej',fieldnames(PHZ.proc))
-        do_restore = true;
-        do_rej = true;
-        
-    else % newThresh == val, oldThresh == 0
-        do_restore = false;
-        do_rej = true;
-        
-    end
+    error('Problem with THRESHOLD.')
 end
+
+% loop trials and mark trials for rejection
+ind = false(size(PHZ.data,1), 1);
+switch lower(units)
+    case 'sd'
+        for i = 1:size(PHZ.data,1)
+            if max(abs(PHZ.data(i,:))) > std(PHZ.data(i,:)) * threshold
+                    ind(i) = true;
+            end
+        end
+            
+    case lower(PHZ.units)
+        for i = 1:size(PHZ.data,1)
+            if max(abs(PHZ.data(i,:))) > threshold
+                ind(i) = true;
+            end
+        end
+end
+    
+% check that ~reject all trials or ~reject no trials
+if sum(ind) == size(PHZ.data,1)
+    fprintf('! This threshold would reject all trials. Aborting phz_reject.m...\n')
+    fprintf('  The min threshold to reject a trial in this dataset is %f.', ...
+        min(max(abs(PHZ.data), [], 2)))
+    return
+end
+
+if sum(ind) == 0
+    fprintf('! This threshold would not reject any trials. Aborting phz_reject.m...\n')
+    fprintf('  The max value in this dataset is %f.', ...
+        max(abs(PHZ.data(:))))
+    return
+end
+
+% add info to proc structure
+PHZ.proc.reject.threshold = threshold;
+PHZ.proc.reject.units = units;
+PHZ.proc.reject.ind = ind;
+
+% add to PHZ.history
+historyThreshold = [num2str(threshold),' ',PHZ.proc.reject.units];
+PHZ = phz_history(PHZ,['Threshold of ', historyThreshold, ' rejected ', ...
+    num2str(sum(PHZ.proc.reject.ind)),' / ', num2str(length(PHZ.proc.reject.ind)), ...
+    ' trials (', num2str(round(phz_rejrate(PHZ,'%'),1)), '%).'], verbose);
+    
 end
 
 function rejrate = phz_rejrate(PHZ,varargin)
@@ -230,8 +144,8 @@ for i = 1:length(varargin)
     valType = varargin{i};
 end
 
-totalNumTrials = size(PHZ.data,1) + length(PHZ.proc.rej.locs);
-totalRejTrials = length(PHZ.proc.rej.locs);
+totalNumTrials = size(PHZ.data,1);
+totalRejTrials = sum(PHZ.proc.reject.ind);
 
 rejrate = totalRejTrials / totalNumTrials;
 
@@ -240,17 +154,4 @@ switch lower(valType)
     case {'%','percent'}, rejrate = rejrate * 100;
     otherwise, warning('Unrecognized valType. Returning proportion.')
 end
-end
-
-function PHZ = getRejStructure(PHZ)
-PHZ.proc.rej.threshold = [];
-PHZ.proc.rej.units = '';
-PHZ.proc.rej.data = [];
-PHZ.proc.rej.locs = [];
-PHZ.proc.rej.data_locs = transpose(1:size(PHZ.data,1));
-PHZ.proc.rej.participant = categorical;
-PHZ.proc.rej.condition = categorical;
-PHZ.proc.rej.group = categorical;
-PHZ.proc.rej.session = categorical;
-PHZ.proc.rej.trials = categorical;
 end
