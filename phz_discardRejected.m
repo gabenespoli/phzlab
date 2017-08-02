fcc5c30a00ba52911ea0d2fedab04b5fb6831b21
Gabe Nespoli <gabenespoli@gmail.com>
2017-08-02 19:16:08 -0400

    notes about how this function should be called from others

---
+++ phz_discardRejected.m
@@ -0,0 +1,26 @@
+% this function should be called by these functions before they 
+%   do anything else:
+% - phz_summary
+% - phz_feature
+% - phz_plot
+% - phz_writetable
+%
+% this function will actually get rid of trials that are marked
+%   for rejection in:
+% - PHZ.reject.ind
+% - PHZ.reject.manual
+% - PHZ.subset.ind (check for multiple of these in PHZ.proc)
+%
+% issues:
+% - should the rejections from the different sources be done in
+%   the order they appear, or some other order?
+%
+% - could find indices of any of them, loop them, and switch/case
+%   them to do the rejections
+%
+% - maybe phz_plotTrials should be called something else and have 
+%   its own proc field. phz_reviewTrials? PHZ.proc.review
+%
+% - the proc fields that are actually dependant on each other
+%   such that it matters which order they are done in are:
+%   reject, blsub, norm, transform
