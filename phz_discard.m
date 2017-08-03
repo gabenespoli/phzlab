c95299b2138e8f5f197e00ed2c8d4015de63e6d4
Gabe Nespoli <gabenespoli@gmail.com>
2017-08-03 16:07:25 -0400

    make the function actually reject trials from data, tags, resp

--- phz_discard.m
+++ phz_discard.m
@@ -1,6 +1,6 @@
 %PHZ_DISCARD  Actually remove trials that are marked for rejection.
-%   Trials can be marked using phz_reject, phz_subset, and 
-%   phz_review. This function is primarily used by phz_summary,
+%   Trials can be marked using phz_reject, phz_review, and 
+%   phz_subset. This function is primarily used by phz_summary,
 %   phz_feature, phz_plot, and phz_writetable.
 %
 % USAGE
@@ -12,8 +12,8 @@
 %   At least one of the following fields should be populated using
 %   the appropriate function:
 %       PHZ.proc.reject.keep
-%       PHZ.proc.subset.keep
 %       PHZ.proc.review.keep
+%       PHZ.proc.subset.keep
 %
 % OUTPUT
 %   PHZ.data  = Data with marked trials removed.
@@ -40,34 +40,48 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
 
-function PHZ = phz_discard(PHZ)
+function PHZ = phz_discard(PHZ, verbose)
 
 if nargin == 0 && nargout == 0, help phz_discard, return, end
+if nargin < 2, verbose = true; end
 
+% all of the 'keep' fields have 1 = keep and 0 = discard
+% so, to combine them all we should use &
+keep = true(size(PHZ.data,1), 1);
 
+% combine all 'keep' vectors with logical AND
+% i.e., if at least one of the vectors has a 0 for a trial,
+%   then it will be discarded
+names = fieldnames(PHZ.proc);
+ind = find(contains(names,{'reject', 'review', 'subset'}));
+for i = 1:length(ind)
+    keep = keep & PHZ.proc.(names{ind(i)}).keep;
+end
+procs = strjoin(names(ind),' ');
 
+% actually reject the trials
+PHZ.data = PHZ.data(keep, :);
 
+for fields = {'participant','group','condition','session','trials'}
+    field = fields{1};
+    PHZ.meta.tags.(field) = PHZ.meta.tags.(field)(keep);
+end
 
+qnum = {'q1','q2','q3','q4','q5'};
+suffix = {'', '_acc', '_rt'};
+for i = 1:length(qnum)
+    for j = 1:length(suffix)
+        field = [qnum{i}, suffix{j}];
+        if ~isempty(PHZ.resp.(field))
+            PHZ.resp.(field) = PHZ.resp.(field)(keep);
+        end
+    end
+end
 
+PHZ.proc.discard.procs = procs;
+PHZ.proc.discard.keep = keep;
 
-
-
-% issues:
-% - should the rejections from the different sources be done in
-%   the order they appear, or some other order?
-%
-% - could find indices of any of them, loop them, and switch/case
-%   them to do the rejections
-%
-% - the proc fields that are actually dependant on each other
-%   such that it matters which order they are done in are:
-%   reject, blsub, norm, transform
-%
-
-
-
-
-
-
+historyStr = ['Discarded trials by ',procs,'.'];
+PHZ = phz_history(PHZ, historyStr, verbose);
 
 end
