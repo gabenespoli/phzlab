17f724f0533aae6a027573f2be6f5fce7864988d
Gabe Nespoli <gabenespoli@gmail.com>
2017-08-03 10:14:16 -0400

    update comments with stuff I've already implemented

--- phz_discardRejected.m
+++ phz_discardRejected.m
@@ -1,3 +1,5 @@
+%PHZ_DISCARD
+
 % this function should be called by these functions before they 
 %   do anything else:
 % - phz_summary
@@ -7,9 +9,9 @@
 %
 % this function will actually get rid of trials that are marked
 %   for rejection in:
-% - PHZ.reject.ind
-% - PHZ.reject.manual
-% - PHZ.subset.ind (check for multiple of these in PHZ.proc)
+% - PHZ.reject.keep
+% - PHZ.review.keep
+% - PHZ.subset.keep (check for multiple of these in PHZ.proc)
 %
 % issues:
 % - should the rejections from the different sources be done in
@@ -18,14 +20,7 @@
 % - could find indices of any of them, loop them, and switch/case
 %   them to do the rejections
 %
-% - maybe phz_plotTrials should be called something else and have 
-%   its own proc field. phz_reviewTrials? PHZ.proc.review
-%
 % - the proc fields that are actually dependant on each other
 %   such that it matters which order they are done in are:
 %   reject, blsub, norm, transform
 %
-% - make all these logical vector functions mark 0 for rejected
-%   and call the field 'keep'
-%
-% - in phz_reviewTrials show all types of rejection; show line for threshold rejection?
