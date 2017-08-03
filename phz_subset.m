3a473080211f66d251182cda87980ecaa5f4e9ac
Gabe Nespoli <gabenespoli@gmail.com>
2017-08-03 08:53:00 -0400

    revamp to just save the logical vector in the proc field

--- phz_subset.m
+++ phz_subset.m
@@ -1,32 +1,41 @@
-%PHZ_SUBSET  Extract a subset of data from a PHZ or PHZS structure.
+%PHZ_SUBSET  Extract a subset of data from a PHZ structure, either by
+%   specifying values of grouping variables, or by a logical vector.
 %
 % USAGE    
-%   PHZ = phz_subset(PHZ,subset)
-%   PHZ = phz_subset(PHZ,ind)
+%   PHZ = phz_subset(PHZ, subset)
 %
 % INPUT
 %   PHZ       = [struct] PHZLAB data structure.
 % 
-%   subset    = {'string' value} A cell array of length 2, where the first
-%               value is a field to restrict by (i.e., 'participant', 
-%               'group', 'condition', 'session', 'trials', or a PHZ.resp 
-%               field) and the second item is a number, string, or cell
-%               array of strings with the value(s) of the field to include.
-% 
-%   ind       = A logcial vector (i.e., 1's & 0's) the same length as the
-%               number of trials. Positions with a 1 are included,
+%   subset    = [cell|logical|numeric] If cell array of length 2, the
+%               first value is a field to restrict by (i.e., 'participant',
+%               'group', 'condition', 'session', 'trials') and the 
+%               second item is a number, string, or cell array of
+%               strings with the value(s) of the field to include.
+%           
+%               If a logcial vector (i.e., 1's & 0's) the same length 
+%               as the number of trials. Positions with a 1 are included,
 %               positions with a 0 are excluded.
 %
+%               If a numeric vector, values cannot be below zero or 
+%               greater than the number of trials. Values that are 
+%               specified are included.
+%
 % OUTPUT
-%       PHZ.proc.subset.type
-%       PHZ.proc.subset.ind
+%   PHZ.proc.subset.input = If cell array input, it is copied here 
+%               for reference.
+%
+%   PHZ.proc.subset.keep = Logical vector the same length as the number
+%               of trials, where 1's are included and 0's are not.
 %
 % EXAMPLES
-%   phz_subset(PHZ,{'session' '1'}) >> Only include data from session 1.
-%   phz_subset(PHZ,{'acc' '1'})     >> Only include data from trials with
-%                                      an accurate response.
-%   phz_subset(PHZ,PHZ.resp.q1_rt < 10) >> Only include data from trials
-%                                      with a reaction time less than 10 s.
+%   phz_subset(PHZ,{'group','control'})  >> Only include data from the 
+%                                           control group.
+%   phz_subset(PHZ,{'session',1})        >> Only include data from session 1.
+%   phz_subset(PHZ,PHZ.resp.q1_acc == 1) >> Only include data from trials
+%                                           with an accurate response.
+%   phz_subset(PHZ,PHZ.resp.q1_rt < 9)   >> Only include data from trials
+%                                           with reaction time less than 9 s.
 
 % Copyright (C) 2016 Gabriel A. Nespoli, gabenespoli@gmail.com
 % 
@@ -43,43 +52,38 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
 
-function PHZ = phz_subset(PHZ,subset,verbose)
+function PHZ = phz_subset(PHZ, subset, verbose)
 
 if nargout == 0 && nargin == 0, help phz_subset, return, end
 if isempty(subset), return, end
 if nargin < 3, verbose = true; end
 
 % get indices to keep
-if isnumeric(subset) || islogical(subset)
-    inddata = subset;
+if islogical(subset) || ...
+    (isnumeric(subset) ...
+    && length(subset) == size(PHZ.data,1) ...
+    && all(ismember(subset, [true false])))
     if length(subset) ~= size(PHZ.data,1)
-        error('Index vector is an invalid length.')
+        error('Logical vector must be the same length as the number of trials.')
     end
+    subsetInd = subset;
+    subsetInput = '';
+    subsetStr = 'Restricted data by indices.';
+
+elseif isnumeric(subset)
+    if any(subset <= 0) || any(subset > size(PHZ.data,1))
+        error('Indices cannot be below zero or larger than the number of trials.')
+    end
+    ind = false(size(PHZ.data,1), 1);
+    ind(subset) = true;
+    subsetInput = '';
     subsetStr = 'Restricted data by indices.';
 
 elseif iscell(subset)
-    [field,labels] = verifySubsetInput(PHZ,subset);
+    subsetInput = subset;
+    [field, labels] = verifySubsetInput(PHZ, subset);
 
-    if strcmp(field,'acc'), field = 'acc1'; end
-    switch field
-        case {'acc1','acc2','acc3','acc4','acc5'},
-            inddata = ismember(PHZ.resp.(['q',field(4),'_acc']),labels);
-
-        case {'participant','group','condition','session','trials'}
-            inddata = ismember(PHZ.meta.tags.(field),labels);
-            if ismember('rej',fieldnames(PHZ.proc))
-                indrej = ismember(PHZ.proc.rej.(field),labels);
-                indall = nan(length(PHZ.proc.rej.locs) + length(PHZ.proc.rej.data_locs),1);
-                indall(inddata) = inddata;
-                indall(indrej) = indrej;
-            else
-                indrej = [];
-                indall = inddata;
-            end
-
-        otherwise
-            error('Invalid field by which to restrict.')
-    end
+    ind = ismember(PHZ.meta.tags.(field), labels);
 
     if isnumeric(labels), labels = num2str(labels); end
     subsetStr = ['Restricted data to: ',field,' = ',...
@@ -89,69 +93,34 @@ else
     error('Invalid input.')
 end
 
-inddata = logical(inddata);
-indall = logical(indall);
-indrej = logical(indrej);
-
-% adjust tags
-for i = {'participant','group','condition','session','trials'}, field = i{1};
-    if ~isempty(PHZ.(field))
-        PHZ.meta.tags.(field) = PHZ.meta.tags.(field)(inddata);
-    end
-end
-
-% adjust grouping vars (unique vals of tags)
-for i = {'participant','group','condition','session','trials'}, field = i{1};
-    if length(PHZ.(field)) ~= length(unique(PHZ.meta.tags.(field)))
-        uniqueInd = ismember(PHZ.(field), unique(PHZ.meta.tags.(field)));
-        PHZ.(field) = PHZ.(field)(uniqueInd);
-        PHZ.meta.spec.(field) = PHZ.meta.spec.(field)(uniqueInd);
-    end
-end
-
-% adjust values in data, tags, grouping vars, and rej
-PHZ.data = PHZ.data(inddata,:);
-if ismember('rej',fieldnames(PHZ.proc))
-    PHZ.proc.rej.data = PHZ.proc.rej.data(indrej,:);
-end
-
-% adjust values in PHZ.blc
-if ismember('blc',fieldnames(PHZ.proc))
-    PHZ.proc.blc.values = PHZ.proc.blc.values(indall);
-end
-
-% adjust values in PHZ.resp
-for i = 1:5
-    qx = ['q',num2str(i)];
-    if ~isempty(PHZ.resp.(qx))
-        PHZ.resp.(qx) = PHZ.resp.(qx)(indall);
-        PHZ.resp.([qx,'_acc'])  = PHZ.resp.([qx,'_acc'])(indall);
-        PHZ.resp.([qx,'_rt'])   = PHZ.resp.([qx,'_rt'])(indall);
-    end
-end
-
 % add to history
 PHZ = phz_history(PHZ,subsetStr,verbose);
+procName = phzUtil_getUniqueProcName(PHZ,'subset');
+PHZ.proc.(procName).input = subsetInput;
+PHZ.proc.(procName).keep = ind;
 
 end
 
 function [field,labels] = verifySubsetInput(PHZ,subset)
 
 if length(subset) ~= 2
-    error('SUBSET cell array input must be of length 2.')
+    error('Cell array input must be of length 2.')
 end
 
 field = subset{1};
 labels = subset{2};
 
 if ~ischar(field)
-    error('The first item in SUBSET must be a string.')
+    error('The first item in subset cell array input must be a string.')
 end
 if ~ismember(field, {'participant','group','condition','session','trials'})
-    error(['The first item in SUBSET must be a grouping variable, i.e., ', ...
-       'participant, group, condition, session, or trials.'])
+    error(['The first item in subset cell array input must be a grouping ', ...
+       'variable; i.e., participant, group, condition, session, or trials.'])
 end
 
+if isnumeric(labels)
+    labels = num2str(labels);
+end
 if ischar(labels)
     labels = cellstr(labels);
 end
