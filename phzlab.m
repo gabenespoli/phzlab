3115affb7b20bac9d1b32925faeb7f2a4d21532c
Gabe Nespoli <gabenespoli@gmail.com>
2018-01-15 10:54:56 -0500

    phzlab.m: display readme in command window

--- phzlab.m
+++ phzlab.m
@@ -1,12 +1,7 @@
-%PHZLAB  A MATLAB toolbox for analysis of physiological data.
-% PHZ structures are created for each recording of data, and can be
-%   filtered, epoched, and otherwise preprocessed. Multiple PHZ structures
-%   can be combined into a single one, enabling powerful plotting
-%   functionality. Data can then be exported into a table for statistical
-%   analyses.
-% 
-% Version 0.9 (beta).
-% 
+%PHZLAB  A MATLAB toolbox for analysis of physiological data, both
+% peripheral (e.g., SCL, EMG) and neural (e.g., ABR, FFR).
+% Run `phzlab` or view the README for more information.
+
 % Copyright (C) 2018 Gabriel A. Nespoli, gabenespoli@gmail.com
 % 
 % This program is free software: you can redistribute it and/or modify
@@ -21,33 +16,5 @@
 % 
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
-% 
-% FILE I/O
-%   phz_create:      Create a PHZ structure from a data file.
-%   phz_combine:     Combine many PHZ structures into a single one.
-%   phz_save:        Save a PHZ structure.
-%   phz_load:        Load a PHZ structure.
-%   phz_field:       Change the values of certain PHZ structure fields.
-% 
-% PROCESSING
-%   phz_transform:   Transform data (e.g., square root, etc.)
-%   phz_filter:      Butterworth filtering.
-%   phz_rectify:     Full- or half-wave rectification.
-%   phz_smooth:      Sliding window averaging (incl. RMS)
-%   phz_epoch:       Split a single channel of data into trials.
-%   phz_trials:      Add names to each trial of epoched data.
-%   phz_rej:         Remove trials with values exceeding a threshold or SD.
-%   phz_blsub:       Subtract mean of baseline region from each trial.
-%   phz_norm:        Normalize across specified grouping variables.
-% 
-% ANALYSIS
-%   phz_subset:      Keep data only from specified grouping variables.
-%   phz_region:      Keep only data from a certain time region.
-%   phz_feature:     Convert data to the specified feature.
-%   phz_summary:     Average across grouping variables.
-% 
-% EXPORTING
-%   phz_plot:        Visualize data as line or bar graphs.
-%   phz_writetable:  Export features to a MATLAB table or csv file.
 
-function phzlab, help phzlab, end
+function phzlab, type('README.md'), end
