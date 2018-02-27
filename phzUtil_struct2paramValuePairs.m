ceff4758f366b42aeefbc3c69cd787e4a79be8d9
Gabe Nespoli <gabenespoli@gmail.com>
2018-02-27 13:26:01 -0500

    phzUtil_struct2paramValuePairs.m: initial commit

---
+++ phzUtil_struct2paramValuePairs.m
@@ -0,0 +1,22 @@
+function C = phzUtil_struct2paramval(S)
+% convert a struct S to a cell of parameter-value pairs C.
+% e.g., the struct
+%       S.name = 'Gabe'
+%       S.id = 3
+% becomes
+%       C = {'name', 'Gabe', 'id', 3}
+
+if ~isstruct(S)
+    error('Input must be a struct.')
+end
+
+names = fieldnames(S);
+C = cell(1, length(names) * 2);
+
+for i = 1:length(names)
+    ind = 1 + (i - 1) * 2;
+    C{ind}   = names{i}; % param
+    C{ind+1} = S.(names{i}); % value
+end
+
+end
