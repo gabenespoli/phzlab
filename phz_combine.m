5252523bd166c8486484b70c0a1fb76af9a1a4d9
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-17 10:57:19 -0400

    phz_combine.m: change PHZ to TMP and PHZS to PHZ

--- phz_combine.m
+++ phz_combine.m
@@ -1,4 +1,4 @@
-%PHZ_COMBINE  Create a PHZS structure of data from many PHZ structures.
+%PHZ_COMBINE  Create a PHZ structure of data from many PHZ structures.
 % 
 % USAGE    
 %   PHZ = phz_combine
@@ -54,7 +54,7 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see http://www.gnu.org/licenses/.
 
-function PHZS = phz_combine(varargin)
+function PHZ = phz_combine(varargin)
 
 if nargout == 0 && nargin == 0, help phz_combine, return, end
 
@@ -122,93 +122,93 @@ for j = 1:length(files)
     if verbose, disp(['Loading data from file ', fileProgress]), end
     
     currentFile = fullfile(folder, files{j});
-    PHZ = phz_load(currentFile, verbose); % runs phz_check.m
+    TMP = phz_load(currentFile, verbose); % runs phz_check.m
     
     % do user-defined preprocessing
     for i = 1:2:length(processing)
         switch lower(processing{i})
-            case 'subset',                  PHZ = phz_subset(PHZ,   processing{i+1}, verbose);
-            case {'rect','rectify'},        PHZ = phz_rect(PHZ,     processing{i+1}, verbose);
-            case {'filter','filt'},         PHZ = phz_filter(PHZ,   processing{i+1}, 'verbose', verbose);
-            case {'smooth','smoothing'},    PHZ = phz_smooth(PHZ,   processing{i+1}, verbose);
-            case 'transform',               PHZ = phz_transform(PHZ,processing{i+1}, verbose);
-            case 'blsub',                   PHZ = phz_blsub(PHZ,    processing{i+1}, verbose);
-            case {'rej','reject'},          PHZ = phz_reject(PHZ,   processing{i+1}, verbose);
-            case {'norm','normtype'},       PHZ = phz_norm(PHZ,     processing{i+1}, verbose);
+            case 'subset',                  TMP = phz_subset(TMP,   processing{i+1}, verbose);
+            case {'rect','rectify'},        TMP = phz_rect(TMP,     processing{i+1}, verbose);
+            case {'filter','filt'},         TMP = phz_filter(TMP,   processing{i+1}, 'verbose', verbose);
+            case {'smooth','smoothing'},    TMP = phz_smooth(TMP,   processing{i+1}, verbose);
+            case 'transform',               TMP = phz_transform(TMP,processing{i+1}, verbose);
+            case 'blsub',                   TMP = phz_blsub(TMP,    processing{i+1}, verbose);
+            case {'rej','reject'},          TMP = phz_reject(TMP,   processing{i+1}, verbose);
+            case {'norm','normtype'},       TMP = phz_norm(TMP,     processing{i+1}, verbose);
         end
     end
     
-    PHZ = phzABR_equalizeTrials(PHZ, equalGrpVar, verbose);
-    PHZ = phz_region(PHZ, region, verbose);
-    PHZ = phz_summary(PHZ, keepVars, verbose);
+    TMP = phzABR_equalizeTrials(TMP, equalGrpVar, verbose);
+    TMP = phz_region(TMP, region, verbose);
+    TMP = phz_summary(TMP, keepVars, verbose);
     
     % combine data
     % -----------
     if j == 1
-        PHZS = PHZ;
+        PHZ = TMP;
         
         % reset history field
-        PHZS.history = {};
-        PHZS = phz_history(PHZS,'Combined PHZ structure created.', verbose);
-        PHZS = phz_history(PHZS,['Preprocessing: ', strnumjoin(processing)], verbose);
-        PHZS.lib.files{j} = currentFile;
-        if ismember('filename', fieldnames(PHZ.lib))
-            PHZ.lib = rmfield(PHZ.lib, 'filename'); end
+        PHZ.history = {};
+        PHZ = phz_history(PHZ,'Combined PHZ structure created.', verbose);
+        PHZ = phz_history(PHZ,['Preprocessing: ', strnumjoin(processing)], verbose);
+        PHZ.lib.files{j} = currentFile;
+        if ismember('filename', fieldnames(TMP.lib))
+            TMP.lib = rmfield(TMP.lib, 'filename'); end
         
-        PHZS.proc = [];
-        PHZS.proc.combine{j} = PHZ.proc;
-        PHZS.etc.combine{j} = PHZ.etc;
+        PHZ.proc = [];
+        PHZ.proc.combine{j} = TMP.proc;
+        PHZ.etc.combine{j} = TMP.etc;
 
         continue
     end    
     
     % make sure data length is compatible
-    if size(PHZS.data,2) ~= size(PHZ.data,2)
-        PHZS = phz_history(PHZS, ['NOTE: The length of the data in ''', ...
+    if size(PHZ.data,2) ~= size(TMP.data,2)
+        PHZ = phz_history(PHZ, ['NOTE: The length of the data in ''', ...
             files{j}, ''' was different, so it was not included.'], verbose, 0);
         continue
     end
     
     % make sure sampling frequency is compatible
-    if PHZ.srate ~= PHZS.srate
-        PHZS = phz_history(PHZS, ['NOTE: The ''srate'' field of ''', ...
-            files{j}, ''' is different (', num2str(PHZ.(field)), ...
+    if TMP.srate ~= PHZ.srate
+        PHZ = phz_history(PHZ, ['NOTE: The ''srate'' field of ''', ...
+            files{j}, ''' is different (', num2str(TMP.(field)), ...
             '), so it was not included.'], verbose, 0);
         continue
     end
     
     % proc field
-    PHZS.proc.combine{j} = PHZ.proc;
-    PHZS.etc.combine{j} = PHZ.etc;
+    PHZ.proc.combine{j} = TMP.proc;
+    PHZ.etc.combine{j} = TMP.etc;
     
     % basic fields (strings)
-    PHZS.lib.files{j} = currentFile;
+    PHZ.lib.files{j} = currentFile;
     for i = {'study','datatype','units'}, field = i{1};
-        if ~strcmp(PHZ.(field),PHZS.(field))
-            PHZS = phz_history(PHZS, ['NOTE: The ''',field,''' field of ''', ...
-                files{j}, ''' is different: ''', PHZ.(field),'''.'], verbose, 0);
+        if ~strcmp(TMP.(field),PHZ.(field))
+            PHZ = phz_history(PHZ, ['NOTE: The ''',field,''' field of ''', ...
+                files{j}, ''' is different: ''', TMP.(field),'''.'], verbose, 0);
         end
     end
 
     % grouping variables & tags
     % if different, reset to include unique values of tags after looping
     for i = {'participant','group','condition','session','trials'}, field = i{1};
-        if ~strcmp(PHZ.lib.tags.(field),'<collapsed>')
+        if ~strcmp(TMP.lib.tags.(field),'<collapsed>')
             
-            if ~all(ismember(cellstr(PHZ.(field)),cellstr(PHZS.(field))))
+            if ~all(ismember(cellstr(TMP.(field)),cellstr(PHZ.(field))))
                 if ~ismember(field,resetFields), resetFields{end+1} = field; end %#ok<AGROW>
             end
             
+            TMP.lib.tags.(field) = categorical(cellstr(TMP.lib.tags.(field)),'Ordinal',false);
             PHZ.lib.tags.(field) = categorical(cellstr(PHZ.lib.tags.(field)),'Ordinal',false);
-            PHZS.lib.tags.(field) = categorical(cellstr(PHZS.lib.tags.(field)),'Ordinal',false);
-            PHZS.lib.tags.(field) = [PHZS.lib.tags.(field); PHZ.lib.tags.(field)];
+            PHZ.lib.tags.(field) = [PHZ.lib.tags.(field); TMP.lib.tags.(field)];
             
         end
     end
     
     % data
-    PHZS.data = [PHZS.data; PHZ.data];
-    if numel(PHZS.data) > 50000000 % check if filesize is getting too big
+    PHZ.data = [PHZ.data; TMP.data];
+    if numel(PHZ.data) > 50000000 % check if filesize is getting too big
         error(['Too much data to combine into one variable. Consider ',...
             'doing some preprocessing and averaging with phz_combine.'])
     end
@@ -216,61 +216,61 @@ for j = 1:length(files)
     % behavioural response data
     for i = 1:5
         qx = ['q',num2str(i)];
-        PHZS.resp.(qx) = [PHZS.resp.(qx); PHZ.resp.(qx)];
-        PHZS.resp.([qx,'_acc']) = [PHZS.resp.([qx,'_acc']); PHZ.resp.([qx,'_acc'])];
-        PHZS.resp.([qx,'_rt']) = [PHZS.resp.([qx,'_rt']); PHZ.resp.([qx,'_rt'])];
+        PHZ.resp.(qx) = [PHZ.resp.(qx); TMP.resp.(qx)];
+        PHZ.resp.([qx,'_acc']) = [PHZ.resp.([qx,'_acc']); TMP.resp.([qx,'_acc'])];
+        PHZ.resp.([qx,'_rt']) = [PHZ.resp.([qx,'_rt']); TMP.resp.([qx,'_rt'])];
     end
 
-    PHZS = verifyFieldsThatShouldBeTheSame(PHZS,PHZ,j);
+    PHZ = verifyFieldsThatShouldBeTheSame(PHZ,TMP,j);
     
     if verbose, disp(' '), end
 end % end looping participants
 
-% cleanup PHZS
+% cleanup PHZ
 % ------------
 for j = 1:length(resetFields), field = resetFields{j};
-    PHZS.(field) = unique(PHZS.lib.tags.(field));
-    PHZS = phz_history(PHZS,['The ',field, ...
+    PHZ.(field) = unique(PHZ.lib.tags.(field));
+    PHZ = phz_history(PHZ,['The ',field, ...
         ' field was reset to include the unique values of tags.', ...
         field,'.'],verbose,0);
 end
 
 % save to file (& phz_check)
 if ~isempty(filename)
-    PHZS = phz_save(PHZS,filename);
+    PHZ = phz_save(PHZ,filename);
 else
-    PHZS = phz_check(PHZS);
+    PHZ = phz_check(PHZ);
 end
 
 end
 
-function PHZS = verifyFieldsThatShouldBeTheSame(PHZS,PHZ,i)
+function PHZ = verifyFieldsThatShouldBeTheSame(PHZ,TMP,i)
 if i == 1
     
-    if ismember('times',fieldnames(PHZ)),     PHZS.times = PHZ.times;
-    elseif ismember('freqs',fieldnames(PHZ)), PHZS.freqs = PHZ.freqs;
+    if ismember('times',fieldnames(TMP)),     PHZ.times = TMP.times;
+    elseif ismember('freqs',fieldnames(TMP)), PHZ.freqs = TMP.freqs;
     end
     
-    rname = fieldnames(PHZ.region);
+    rname = fieldnames(TMP.region);
     for j = 1:length(rname)
-        PHZS.region.(rname{j}) = PHZ.region.(rname{j});
+        PHZ.region.(rname{j}) = TMP.region.(rname{j});
     end
     
 else
-    if ismember('times',fieldnames(PHZS))
-        if ~all(PHZS.times == PHZ.times)
+    if ismember('times',fieldnames(PHZ))
+        if ~all(PHZ.times == TMP.times)
             error('PHZ.times is inconsistent.')
         end
-    elseif ismember('freqs',fieldnames(PHZS)), PHZS.freqs = PHZ.freqs;
-        if ~all(PHZS.freqs == PHZ.freqs)
+    elseif ismember('freqs',fieldnames(PHZ)), PHZ.freqs = TMP.freqs;
+        if ~all(PHZ.freqs == TMP.freqs)
             error('PHZ.freqs is inconsistent.')
         end
     end
     
-    rname = fieldnames(PHZS.region);
+    rname = fieldnames(PHZ.region);
     for j = 1:length(rname)
-        if ~all(PHZS.region.(rname{j}) == PHZ.region.(rname{j}))
-            error(['FFR.region.',(rname{j}),' is inconsistent.'])
+        if ~all(PHZ.region.(rname{j}) == TMP.region.(rname{j}))
+            error(['PHZ.region.',(rname{j}),' is inconsistent.'])
         end
     end
 end
